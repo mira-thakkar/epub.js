ef4f74be5f61413bcca38cac19b1afb5aca0c06b
Mira Thakkar <mirathakkar94@gmail.com>
2017-12-09 13:03:43 +0530

    fix

--- karma.conf.js
+++ karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function(config) {
         loaders: [
           {
             test: /\.js$/,
-            exclude: /node_modules\/(?!(marks-pane)\/).*/,
+            exclude: /node_modules\/(?!()\/).*/,
             loader: "babel-loader",
             query: {
               presets: ['es2015'],
